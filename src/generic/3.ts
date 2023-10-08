bbea9e21f89b122711b1ef5c90e10601693461c9
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2023-10-08 20:14:27 +0300

    correct mistakes

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,13 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, U> (objA:T, objB:U) {
-  return Object.assign({}, objA, objB);
+// function merge<T, U> (objA:T, objB:U) {
+//   return Object.assign({}, objA, objB);
+// }
+function merge<T extends Record<string, any>, U extends Record<string, any>>(objA: T, objB: U) {
+    if (typeof objA === 'object' && typeof objB === 'object') {
+        return Object.assign({}, objA, objB);
+    } 
 }
 
 const merged = merge({ name: 'Tanya' }, { age: 25 });
